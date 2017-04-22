7bde44492b1b804f397f16aa27d8626690d35eee
wesleythorsen <wesley.thorsen@gmail.com>
2017-04-22 15:01:26 -0700

    initial commit

---
+++ init.c
@@ -0,0 +1,52 @@
+#include "ucode.c"
+int console;
+
+main()
+{
+	int in, out;
+	in = open("/dev/tty0", O_RDONLY);
+	out = open("/dev/tty0", O_WRONLY);
+	printf("INIT: fork a login process in console\n");
+	console = fork();
+	if(console)
+	{
+		parent();
+	}
+	else
+	{
+		login();
+	}
+}
+
+int login()
+{
+	exec("login /dev/tty0");
+}
+
+int parent()
+{
+	int pid, status;
+	
+	while(1)
+	{
+		printf("INIT: wait for ZOMBIE child\n");
+		pid = wait(&status);
+		
+		if (pid == console)
+		{
+			printf("INIT: forks a new console login\n");
+			console = fork();
+			
+			if (console)
+			{
+				continue;
+			}
+			else
+			{
+				login();
+			}
+		}
+		
+		printf("INIT: buried an orphan child %d\n", pid);
+	}
+}
---
+++ q
@@ -0,0 +1,3 @@
+qemu-system-i386 -hda vdisk -m 520m -smp $1 -serial /dev/pts/3 -serial /dev/pts/7 \
+-parallel /dev/pts/8 -localtime 
+
